c25187a343e138b8706f41ea61d0b82bb05988f3
thouel <t.houel@gmail.com>
2023-09-07 15:14:39 +0200

    Removed some secrets

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -1,20 +1,10 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(req) {
-  console.log('in route api/weather GET');
-  console.log(req);
-  // const res = await fetch(
-  //   'http://api.weatherstack.com/current?access_key=3345628948a784f73efdffebc7ba6afb&query=New-York'
-  // );
-  // const data = await res.json();
-  // return NextResponse.json(data);
-}
-
 export async function POST(req) {
   console.log('in route api/weather POST');
   const reqJson = await req.json();
   const town = reqJson.town;
-  const url = `http://api.weatherstack.com/current?access_key=3345628948a784f73efdffebc7ba6afb&query=${town}`;
+  const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=${town}`;
   const res = await fetch(url);
   const data = await res.json();
   return NextResponse.json(data);
